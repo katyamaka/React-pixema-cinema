c19adcceb09d64ff27d3e3a949cd1964dfcfbbf1
katyamaka <katyamaka1998@gmail.com>
2023-05-11 08:53:49 +0300

    feat(ui): add theme.ts (light theme and dark theme)

---
+++ src/ui/theme.ts
@@ -0,0 +1,23 @@
+import { css } from "styled-components";
+
+export const darkTheme = css`
+  --primary: #ffffff;
+  --bg-primary: #000000;
+  --primary-text: #ffffff;
+  --primary-text-light: #afb2b6;
+  --primary-input-bg: #323537;
+  --primary-input-border: #323537;
+  --primary-form-bg: #242426;
+  --label-text: #ffffff;
+`;
+
+export const lightTheme = css`
+  --primary: #000000;
+  --bg-primary: #ffffff;
+  --primary-text: #242426;
+  --primary-text-light: #242426;
+  --primary-input-bg: #ffffff;
+  --primary-input-border: #afb2b6;
+  --primary-form-bg: #ffffff;
+  --label-text: #000000;
+`;
