74ea1527632ffe7ecea53f15268f1be414456314
katyamaka <katyamaka1998@gmail.com>
2023-05-11 08:57:00 +0300

    feat(ui): add barrel pattern in UI

---
+++ src/ui/index.ts
@@ -0,0 +1,8 @@
+import { Color } from "./colors";
+import { Media } from "./media";
+import { darkTheme, lightTheme } from "./theme";
+
+export { Color };
+export { Media };
+export { darkTheme, lightTheme };
+export * from "./typography";
