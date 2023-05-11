0846c7ac475848fb0d60760e37973fcabd3935a3
katyamaka <katyamaka1998@gmail.com>
2023-05-11 09:12:02 +0300

    feat(assets): add empty search result image

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -6,3 +6,4 @@ import { ReactComponent as UserIcon } from "./icons/user.svg";
 import { ReactComponent as BurgerIcon } from "./icons/burger.svg";
 import { ReactComponent as PixemaLightIcon } from "./icons/pixema-logo-light.svg";
 import { ReactComponent as PixemaDarkIcon } from "./icons/pixema-logo-dark.svg";
+import empty from "./images/empty-search-result.png";
