710b59c80dce14414c0d5f6b9991b840d3017dbf
katyamaka <katyamaka1998@gmail.com>
2023-05-23 14:35:21 +0300

    feat(components): add Label Component

---
+++ src/components/Label/style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { Color, S1_3 } from "../../ui";
+
+export const LabelStyled = styled.label`
+  display: inline-block;
+  margin: 0 0 8px 0;
+  color: ${Color.Label_Text};
+  ${S1_3};
+`;
