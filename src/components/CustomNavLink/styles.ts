e6bd0500f439a14b46181f66bdb478ca10d54ba6
katyamaka <katyamaka1998@gmail.com>
2023-05-23 14:06:58 +0300

    feat(components): add CustomNavLink

---
+++ src/components/CustomNavLink/styles.ts
@@ -0,0 +1,30 @@
+import { NavLink, PathMatch } from "react-router-dom";
+import styled from "styled-components";
+import { Color, S1 } from "../../ui";
+
+export const NavLinkStyled = styled(NavLink)<{
+  $isActive: PathMatch<string> | null;
+}>`
+  display: flex;
+  gap: 20px;
+  align-items: center;
+  ${S1}
+  color: ${({ $isActive }) =>
+    $isActive ? `${Color.Primary}` : `${Color.Secondary}`};
+
+  & svg path {
+    stroke: ${({ $isActive }) =>
+      $isActive ? `${Color.Primary}` : `${Color.Secondary}`};
+    fill: ${({ $isActive }) =>
+      $isActive ? `${Color.Primary}` : `${Color.Secondary}`};
+  }
+
+  &:hover {
+    color: ${Color.Primary};
+
+    & svg path {
+      stroke: ${Color.Primary};
+      fill: ${Color.Primary};
+    }
+  }
+`;
