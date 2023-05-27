ad83bccda785dedc8c37a05a20e4d7b508a56416
katyamaka <katyamaka1998@gmail.com>
2023-05-27 12:49:24 +0300

    fix(ui): delete GlobalStyle

--- src/ui/GlobalStyles.ts
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from "styled-components";
-import { resetCSS } from "./resetCSS";
-import { darkTheme, lightTheme } from "./theme";
-
-export const GlobalStyles = createGlobalStyle`
-	${resetCSS}
-	${lightTheme}
-	${darkTheme}
-`;
