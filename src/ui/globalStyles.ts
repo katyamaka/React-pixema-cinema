69eb348916c23b23dfff5257c14e5be0a87d4cdc
katyamaka <katyamaka1998@gmail.com>
2023-05-27 12:50:58 +0300

    Feat(ui): add fixed globalStyle

---
+++ src/ui/globalStyles.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+import { resetCSS } from "./resetCSS";
+import { darkTheme, lightTheme } from "./theme";
+
+export const GlobalStyles = createGlobalStyle`
+  ${resetCSS}
+  ${lightTheme}
+  ${darkTheme}
+`;
